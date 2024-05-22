9508763977febf6f687159620e64b645c34d8192
Denis <astanindenys@gmail.com>
2024-05-22 17:18:08 -0400

    Payment integration(testing)

--- shop/models.py
+++ shop/models.py
@@ -111,7 +111,7 @@ class Review(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['-created_at']
+        ordering = ['created_at']
 
     def __str__(self):
         return f"{self.user.username} - {self.product.name} - {self.rating}"
