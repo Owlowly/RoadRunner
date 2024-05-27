7463a1bbf2de09dfcf8e8d5bda002846b8ac361b
Denis <astanindenys@gmail.com>
2024-05-26 21:28:10 -0400

    Update entire project with latest changes

--- shoes_store/settings.py
+++ shoes_store/settings.py
@@ -163,7 +163,7 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STORAGES = {git remote set-url origin git@
+STORAGES = {
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
